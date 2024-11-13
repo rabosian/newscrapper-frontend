a47f653f2e4dfeb5582b94e2670baaf1c29fa32d
Sunil Park <sunilpark1129@gmail.com>
2024-11-13 03:44:11 -0500

    added - style

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,57 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import './navbar.style.css';
 
 const Navbar = ({ user }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // User 기능이 아직 없기 때문에 임시로 만들어 놓음
+  const [tempUser, setTempUser] = useState(false);
+
+  function handleLogout() {
+    setTempUser(false);
+  }
+
+  function handleLogin() {
+    setTempUser(true);
+
+    // User 기능이 생기면 사용할 것
+    // navigate('/login', { state: { from: location } });
+  }
+
   return (
-    <div className="navbar__container">
-      <Link to="/">
-        <img src="/assets/images/TempLogo.webp" width={200}></img>
+    <header className="navbar">
+      <Link to="/" className="navbar__logo">
+        <div className="image-container">
+          <img src="/assets/images/lowdown-logo.png" />
+        </div>
       </Link>
 
-      {user ? (
-        <>
-          <Link to="/myfavorite">Favorites</Link>
-          <div>
-            <span style={{ cursor: 'pointer' }}>Logout</span>
-          </div>
-        </>
-      ) : (
-        <>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </>
-      )}
-    </div>
+      <nav>
+        <div className="navbar__content">
+          {tempUser ? (
+            <>
+              <button className="navbar__btn" onClick={handleLogout}>
+                Log Out
+              </button>
+              <Link className="navbar__favorite" to="/myfavorite">
+                My Page
+              </Link>
+            </>
+          ) : (
+            <>
+              <button className="navbar__btn" onClick={handleLogin}>
+                Log In
+              </button>
+              <Link className="navbar__register" to="/register">
+                Sign up
+              </Link>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
   );
 };
 
