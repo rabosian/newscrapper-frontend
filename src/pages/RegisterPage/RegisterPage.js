cacddce42c001266f0000f3f6f3c7354d273c208
Sunil Park <sunilpark1129@gmail.com>
2024-11-13 06:52:30 -0500

    updated - register form

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,164 +1,129 @@
 import React, { useEffect, useState } from 'react';
+import './register.style.css';
 import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-
-import './style/register.style.css';
-
+import { Link, useNavigate } from 'react-router-dom';
 import { registerUser } from '../../features/user/userSlice';
 
 const RegisterPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
-  const [firstName, setFirstName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [policy, setPolicy] = useState(false);
-
   const [error, setError] = useState('');
-  const [success, setSuccess] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false); // sign-up 버튼 disable
-  const [isFormValid, setIsFormValid] = useState(false); // sign-up 버튼 disable
 
   // 폼 유효성 체크
-  useEffect(() => {
-    // 비밀번호,비밀번호 확인이 일치하지 않으면 에러 메시지 표시
-    if (password && confirmPassword && password !== confirmPassword) {
-      setError('Passwords do not match.');
-    } else {
-      setError('');
+  function checkFormValid({ password, confirm, policy }) {
+    if (!policy) {
+      setError('You must agree to the Terms and Conditions');
+      return false;
     }
 
-    setIsFormValid(
-      firstName &&
-        email &&
-        password &&
-        confirmPassword &&
-        password === confirmPassword &&
-        policy
-    );
-  }, [firstName, email, password, confirmPassword, policy]);
+    if (!password || !confirm || password !== confirm) {
+      setError('Passwords do not match.');
+      return false;
+    }
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+    return true;
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const [name, email, password, confirm, policy] = e.target;
+    const obj = {
+      name: name.value,
+      email: email.value,
+      password: password.value,
+      confirm: confirm.value,
+      policy: policy.checked,
+    };
+
+    const valid = checkFormValid(obj);
+    // 이상 발견시 진행하지 않고 종료
+    if (!valid) return;
+
     setError('');
-    setSuccess(false);
-
-    if (!isFormValid) return;
-
-    setIsSubmitting(true);
 
     try {
       await dispatch(
         registerUser({
           email,
-          name: firstName,
+          name,
           password,
           navigate,
-          setError,
         })
       ).unwrap();
-      setSuccess(true);
     } catch (error) {
-      setError(error || 'Failed to sign up, please try again.');
+      setError(error);
     } finally {
       setIsSubmitting(false);
     }
   };
 
   return (
-    <div className="register-page">
-      <div className="register-page__box">
-        <h1 className="register-page__title">Sign-up</h1>
+    <div className="register">
+      <div className="register__box">
+        <h1 className="register__title">Sign-up</h1>
 
-        {/* 회원가입 실패시 */}
-        {error && <div className="register-page__error-message">{error}</div>}
-        {/* 회원가입 성공시 */}
-        {success && (
-          <div className="register-page__success-message">
-            Registration successful!
-          </div>
-        )}
-
-        <form className="register-page__form" onSubmit={handleSubmit}>
-          <div className="register-page__form-group">
-            <label htmlFor="formFirstName">Name</label>
+        <form className="register__form" onSubmit={handleSubmit}>
+          <label>
+            Name
             <input
               type="text"
-              id="formFirstName"
-              placeholder="Enter your your name"
-              value={firstName}
+              placeholder="Enter your name"
+              name="name"
               required
-              onChange={(event) => setFirstName(event.target.value)}
-            ></input>
-          </div>
+              autoComplete="off"
+            />
+          </label>
 
-          {/* last name 필드 주석처리 - 백엔드 user schema에 없음*/}
-          {/* <div className="register-page__form-group">
-            <label htmlFor="formLastName">Last Name</label>
-            <input
-              type="text"
-              id="formLastName"
-              placeholder="Enter your last name"
-              onChange={(event) => setLastName(event.target.value)}
-            ></input>
-          </div> */}
-
-          <div className="register-page__form-group">
-            <label htmlFor="formEmail">Email</label>
+          <label>
+            Email
             <input
               type="email"
-              id="formEmail"
               placeholder="Enter your email"
-              value={email}
+              name="email"
               required
-              onChange={(event) => setEmail(event.target.value)}
-            ></input>
-          </div>
-
-          <div className="register-page__form-group">
-            <label htmlFor="formPassword">Password</label>
-            <input
-              type="password"
-              id="formPassword"
-              placeholder="Enter your password"
-              value={password}
-              required
-              onChange={(event) => setPassword(event.target.value)}
-            ></input>
-          </div>
-
-          <div className="register-page__form-group">
-            <label htmlFor="formConfirmPassword">Confirm Password</label>
-            <input
-              type="password"
-              id="formConfirmPassword"
-              placeholder="Re-enter the password"
-              value={confirmPassword}
-              required
-              onChange={(event) => setConfirmPassword(event.target.value)}
-            ></input>
-          </div>
-
-          <div className="register-page__form-group register-page__policy-container">
-            <input
-              type="checkbox"
-              id="policy"
-              checked={policy}
-              onChange={(event) => setPolicy(!policy)}
+              autoComplete="off"
             />
+          </label>
+
+          <label>
+            Password
+            <input
+              type="password"
+              placeholder="Enter your password"
+              name="password"
+              required
+              autoComplete="off"
+            />
+          </label>
+
+          <label>
+            Confirm Password
+            <input
+              type="password"
+              placeholder="Re-enter the password"
+              name="confirm"
+              required
+              autoComplete="off"
+            />
+          </label>
+
+          <div className="register__form-group register__policy-container">
+            <input type="checkbox" id="policy" name="policy" />
             <label htmlFor="policy">I agree to the Terms and Conditions</label>
           </div>
 
-          <button
-            className="register-page__button"
-            type="submit"
-            disabled={!isFormValid || isSubmitting} // isFormValid가 false 또는 isSubmitting이 true이면 버튼 비활성화
-          >
+          {/* 회원가입 실패시 */}
+          {error && <div className="register__error">{error}</div>}
+
+          <button className="register__button" type="submit">
             {isSubmitting ? 'Signing up ...' : 'Register'}
           </button>
         </form>
+        <Link to={'/login'} title="go to login page">
+          Already have an account? Log in here.
+        </Link>
       </div>
     </div>
   );
