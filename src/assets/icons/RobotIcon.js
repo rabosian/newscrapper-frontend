52fe63cc96fe42ca3d23b26674cd27d11739127e
Debora-k <debora0104k@gmail.com>
2024-11-19 23:41:50 -0700

    added favorite button on Modal

---
+++ src/assets/icons/RobotIcon.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function RobotIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 4c-2.21 0 -4 1.79 -4 4v2h-2v7h2v4h8v-4h2v-7h-2v-2c0 -2.21 -1.79 -4 -4 -4zm0 2a2 2 0 0 1 2 2v2h-4v-2a2 2 0 0 1 2 -2zm-3 4h6v3h-6v-3zm-1 5h8v2h-8v-2z" />
+    </svg>
+  );
+}
+
+export default RobotIcon;
--- src/components/article/ArticleModal.js
+++ src/components/article/ArticleModal.js
@@ -4,9 +4,16 @@ import { useSelector } from 'react-redux';
 
 function ArticleModal() {
   const selectedArticle = useSelector((store) => store.article.selectedArticle);
-
+  const favoriteList = useSelector((state) => state.favorite.articleList);
   if (!selectedArticle) return null;
-  return <ArticleDetail article={selectedArticle} />;
+  return (
+    <ArticleDetail
+      article={selectedArticle}
+      isFavorite={favoriteList.find(
+        (favorite) => favorite._id === selectedArticle._id
+      )}
+    />
+  );
 }
 
 export default ArticleModal;
