7c512b978c8942af0984b14c376e3cb02c67a0aa
Sunil Park <sunilpark1129@gmail.com>
2024-11-23 11:35:12 -0500

    fixed - error

--- src/components/common/LoadingComment.js
+++ src/components/common/LoadingComment.js
@@ -3,14 +3,14 @@ import './styles/loadingComment.style.css';
 
 function LoadingComment() {
   return (
-    <d className="loading__comment">
+    <div className="loading__comment">
       <div className="loading__comment-box">
         <div></div>
         <div></div>
         <div></div>
       </div>
       <div className="loading__bg"></div>
-    </d>
+    </div>
   );
 }
 
